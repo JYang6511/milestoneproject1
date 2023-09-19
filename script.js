06ccbefaead245df9839a71790eabb4c801d5c55
Johnson Yang <johnsonyang65@gmail.com>
2023-09-18 20:11:57 -0700

    js brainstorming to add questions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-js
+let questions = [
+    {
+   question: "How many games must be won in a playoff series to move on to the next round?",
+   choices: ["4", "7",  "3", "5"],
+   correctChoice: "4"
+    }
+]
+
+let score = 0;
+let startingQuestionIndex = 0; 
+let questionElement = document.querySelector(".questions")
+questionElement.textContent = "How many games must be won in a playoff series to move on to the next round?";
+
+
