9fb5ba01a062eac20f89a0ae4db5c46c5754d810
Johnson Yang <johnsonyang65@gmail.com>
2023-09-20 20:24:19 -0700

    got questions to display

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-let startButton = document.querySelector(".start")
-let questionContainer = document.querySelector("#questions")
-
 const questions = [
     {
-        question: 'Most points scored by one player in a NBA game?',
-        choices: 
-    }
-]
+        question: "Most points scored by a player in a NBA game?",
+        choices: ["81", "100", "65", "92"],
+        correctAnswer: "100",
+    },
+    {
+        question: "Which NBA player is the NBA logo modeled after?",
+        choices: ["Lebron James", "Michael Jordan", "Jerry West", "Kobe Bryant"],
+        correctAnswer: "Jerry West",
+    },
+    {
+        question: "Which team won the first Super Bowl?",
+        choices: ["Green Bay Packers", "Kansas City Chiefs", "San Francisco 49ers", "Pittsburgh Steelers"],
+        correctAnswer: "Green Bay Packers",
+    },
+];
+
+let startButton = document.querySelector(".start")
+let questionContainer = document.querySelector("#questionsdiv")
+let questionElement = document.querySelector(".questions")
+let currentQuestionIndex = 0 
+
+
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-startButton.classList.add("containerhide")
-questionContainer.classList.remove("containerhide")
-nextQuestion()
+startButton.classList.add("containerhide");
+questionContainer.classList.remove("containerhide");
+currentQuestionIndex = 0;
+nextQuestion(currentQuestionIndex)
 }
 
-function nextQuestion() {
-
+function nextQuestion(questionIndex) {
+   let question = questions[questionIndex]
+questionElement.textContent = question.question;
 
 }
 
 function chooseAnswer() {
 
 }
+
