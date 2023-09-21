7c7bbf395cb63135c9c59553af4b6323f232253e
Johnson Yang <johnsonyang65@gmail.com>
2023-09-20 21:13:55 -0700

    added answers

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const questions = [
     {
         question: "Most points scored by a player in a NBA game?",
-        choices: ["81", "100", "65", "92"],
+        answers: ["81", "100", "65", "92"],
         correctAnswer: "100",
     },
     {
         question: "Which NBA player is the NBA logo modeled after?",
-        choices: ["Lebron James", "Michael Jordan", "Jerry West", "Kobe Bryant"],
+        answers: ["Lebron James", "Michael Jordan", "Jerry West", "Kobe Bryant"],
         correctAnswer: "Jerry West",
     },
     {
         question: "Which team won the first Super Bowl?",
-        choices: ["Green Bay Packers", "Kansas City Chiefs", "San Francisco 49ers", "Pittsburgh Steelers"],
+        answers: ["Green Bay Packers", "Kansas City Chiefs", "San Francisco 49ers", "Pittsburgh Steelers"],
         correctAnswer: "Green Bay Packers",
     },
 ];
@@ -19,6 +19,8 @@ const questions = [
 let startButton = document.querySelector(".start")
 let questionContainer = document.querySelector("#questionsdiv")
 let questionElement = document.querySelector(".questions")
+let answersElement = document.querySelector(".answers")
+console.log(answersElement)
 let currentQuestionIndex = 0 
 
 
@@ -26,15 +28,23 @@ let currentQuestionIndex = 0
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-startButton.classList.add("containerhide");
-questionContainer.classList.remove("containerhide");
-currentQuestionIndex = 0;
-nextQuestion(currentQuestionIndex)
+    startButton.classList.add("containerhide");
+    questionContainer.classList.remove("containerhide");
+    currentQuestionIndex = 0;
+    nextQuestion(currentQuestionIndex)
 }
 
 function nextQuestion(questionIndex) {
-   let question = questions[questionIndex]
-questionElement.textContent = question.question;
+    let question = questions[questionIndex]
+    questionElement.textContent = question.question;
+
+
+    question.answers.forEach((answer, index) => {
+        let answerChoice = document.createElement("li")
+        answerChoice.textContent = answer;
+        answersElement.appendChild(answerChoice)
+        console.log(answerChoice)
+    })
 
 }
 
